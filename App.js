2958591610b76afda436cb16dad8d3f993f0d3fa
anki356 <anki356@gmail.com>
2021-09-21 14:37:10 +0530

    Multiple Events details with all corrections

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import Homestack from './Myassets/Navigation/Homestack';
-//import {Provider} from 'react-redux';
-//import store from './MyAssets/Redux/store';
+
 
 
 
 function App() {
   return (
-    //<Provider store={store}>
+    <View>
     <Homestack/>
-    //</Provider>
+    </View>
   );
 }
 
